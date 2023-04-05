4c3c0b98e2051f6987f2c6ee52bbdfdee4c2d01f
AnujRuhela7 <ruhela7777@gmail.com>
2023-04-05 15:21:22 +0530

    Added Login and Signup Feature

--- index.js
+++ index.js
@@ -3,24 +3,20 @@ if (process.env.NODE_ENV != "prodcution") require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const engine = require("ejs-mate");
+const app = express();
 const mongoose = require("mongoose");
-const Product = require("./models/Product");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const User = require("./models/User");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/User");
-
-const productRouter = require("./routes/productRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
-const authRouter = require("./routes/authRoutes");
 
 // Varaibles
 const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/shopping-app";
 const port = process.env.PORT || 3000;
 const sessionSecret = process.env.SESSION_SECRET || "this is a secret session";
-const app = express();
-const methodOverride = require("method-override");
+
 // Connect to DB
 mongoose
   .connect(dbURL)
@@ -29,8 +25,6 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-mongoose.set("strictQuery", true);
-// const PORT = 3000;
 const sessionflash = {
   secret: sessionSecret,
   resave: false,
@@ -41,34 +35,48 @@ const sessionflash = {
   },
 };
 
-app.use(express.static("public"));
 app.use(session(sessionflash));
 app.use(flash());
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(passport.authenticate("session"));
 
 app.use((req, res, next) => {
-  req.locals.success = req.flash("success");
-  req.locals.error = req.flash("error");
-  req.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
   next();
 });
 
+// Product Routes
+const productRouter = require("./routes/productRoutes");
+// Review Routes
+const reviewRouter = require("./routes/reviewRoutes");
+// Auth Routes
+const authRouter = require("./routes/authRoutes");
+
+mongoose.set("strictQuery", true);
+
+// Middlewares
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
+// Passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Home Route
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Routers
 app.use(productRouter);
 app.use(reviewRouter);
 app.use(authRouter);
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 // app.get("/products", (req, res) => {
 //   res.render("./products/product");
